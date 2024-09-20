7354643b1d6b6ca22691c9c0dae65222a03fa603
Dream <1012377328@qq.com>
2024-09-20 13:49:03 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class=" w-screen h-screen flex gap-4">
+    <div class="w-60 h-full bg-blue" />
+    <div class="flex-1 bg-green">
+      <div />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
